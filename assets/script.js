272668ee23b9e78810c6e19100603ddc59f27b06
emilygknight <emilyknight819@gmail.com>
2023-09-18 17:05:34 -0500

    changing to long lat

--- assets/script.js
+++ assets/script.js
@@ -1,94 +1,71 @@
 var userFormEl = document.querySelector('#user-form');
-var cityInputEl = document.querySelector('#city');
+var cityInput = document.querySelector('#city');
 var weatherContainerEl = document.querySelector('#weather-container');
 var citySearchTerm = document.querySelector('#city-search-term');
-var APIKey = "0d9571e1b0c1a878cfe2800320169226";
-var city;
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+const APIKey = "0d9571e1b0c1a878cfe2800320169226";
+// const description = weatherData.main.temp;
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
 
-  var username = cityInputEl.value.trim();
+  var city = cityInput.value.trim();
 
-  if (username) {
-    getUserCity(username);
+  if (city) {
+    getUserCity(city);
 
     weatherContainerEl.textContent = '';
-    cityInputEl.value = '';
+    cityInput.value = '';
   } else {
     alert('Please enter a City');
   }
 };
 
 var getUserCity = function (city) {
-  var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey;
+  // var queryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey;
 
-  fetch(apiUrl)
-    .then(function (response) {
-      if (response.ok) {
-        console.log(response);
-        response.json().then(function (weather) {
-          console.log(weather);
-          displayCity(weather, city);
-        });
-      } else {
-        alert('Error: ' + response.statusText);
-      }
-    })
-    .catch(function (error) {
-      alert('Unable to connect to the Weather');
-      console.log(error);
-    });
-    console.log(city);
-};
+  // fetch(queryUrl)
+  //   .then(function (response) {
+  //     if (response.ok) {
+  //       console.log(response);
+  //       response.json()
+  //        .then(function (data) {
+  //         console.log(data);
+  //         console.log(data.weather);
+  //         displayCity(data.weather, city);
+  //       });
+  //     } else {
+  //       alert('Error: ' + response.statusText);
+  //     }
+  //   })
+  //   .catch(function (error) {
+  //     alert('Unable to connect to the Weather');
+  //     console.log(error);
+  //   });
+  //   console.log(city);
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey)
+   .then( function (response) {
+     return response.json();
+  })
+  .then (function (data) {
+    const lat = data.coord.lat;
+    const lon = data.coord.lon;
+    console.log('lat', lat);
+    console.log('long', lon);
+    displayCity(data.weather, city);
+    return data;
+  });
 
-// var getFeaturedCity = function (language) {
-//   var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey;
-//   fetch(apiUrl).then(function (response) {
-//     if (response.ok) {
-//       response.json().then(function (data) {
-//         displayCity(data.items, language);
-//       });
-//     } else {
-//       alert('Error: ' + response.statusText);
-//     }
-//   }); ``
-// };
 
-var displayCity = function (city, searchTerm) {
-  // if (city.length === 0) {
-  //   weatherContainerEl.textContent = 'City not found.';
-  //   return;
-  // }
 
-  citySearchTerm.textContent = searchTerm;
 
-  for (var i = 0; i < city.length; i++) {
-    var cityName = city[i].weather + '/' + city[i].name;
+var displayCity = function (city, weather) {
 
-    var cityEl = document.createElement('a');
-    cityEl.classList = 'list-item flex-row justify-space-between align-center';
-    cityEl.setAttribute('href', './single-repo.html?repo=' + cityName);
+console.log(city, weather);
+console.log(city);
+console.log(weather);
+}
 
-    var titleEl = document.createElement('span');
-    titleEl.textContent = cityName;
-
-    cityEl.appendChild(titleEl);
-
-    var statusEl = document.createElement('span');
-    statusEl.classList = 'flex-row align-center';
-
-    // if (city[i].weather > 0) {
-    //   statusEl.innerHTML =
-    //     "<i class='fas fa-times status-icon icon-danger'></i>" + city[i].weather + ' issue(s)';
-    // } else {
-    //   statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    // }
-
-    cityEl.appendChild(statusEl);
-
-    weatherContainerEl.appendChild(cityEl);
-  }
 };
 
 userFormEl.addEventListener('submit', formSubmitHandler);
