bb7a42e5a436fb650ae7a47968552e32b461529a
emilygknight <emilyknight819@gmail.com>
2023-09-18 14:46:00 -0500

    editing out parts i dont need

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ var cityInputEl = document.querySelector('#city');
 var weatherContainerEl = document.querySelector('#weather-container');
 var citySearchTerm = document.querySelector('#city-search-term');
 var APIKey = "0d9571e1b0c1a878cfe2800320169226";
-
+var city;
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
@@ -20,16 +20,6 @@ var formSubmitHandler = function (event) {
   }
 };
 
-// var buttonClickHandler = function (event) {
-//   var language = event.target.getAttribute('data-language');
-
-//   if (language) {
-//     getFeaturedCity(language);
-
-//     cityContainerEl.textContent = '';
-//   }
-// };
-
 var getUserCity = function (city) {
   var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey;
 
@@ -37,9 +27,9 @@ var getUserCity = function (city) {
     .then(function (response) {
       if (response.ok) {
         console.log(response);
-        response.json().then(function (data) {
-          console.log(data);
-          displayCity(data, city);
+        response.json().then(function (weather) {
+          console.log(weather);
+          displayCity(weather, city);
         });
       } else {
         alert('Error: ' + response.statusText);
@@ -47,57 +37,58 @@ var getUserCity = function (city) {
     })
     .catch(function (error) {
       alert('Unable to connect to the Weather');
+      console.log(error);
     });
     console.log(city);
 };
 
-var getFeaturedCity = function (language) {
-  var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey;
-  fetch(apiUrl).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        displayCity(data.items, language);
-      });
-    } else {
-      alert('Error: ' + response.statusText);
-    }
-  });
-};
-
-// var displayCity = function (city, searchTerm) {
-//   if (city.length === 0) {
-//     cityContainerEl.textContent = 'City not found.';
-//     return;
-//   }
-
-//   citySearchTerm.textContent = searchTerm;
-
-//   for (var i = 0; i < city.length; i++) {
-//     var cityName = city[i].owner.login + '/' + city[i].name;
-
-//     var cityEl = document.createElement('a');
-//     cityEl.classList = 'list-item flex-row justify-space-between align-center';
-//     cityEl.setAttribute('href', './single-repo.html?repo=' + cityName);
-
-//     var titleEl = document.createElement('span');
-//     titleEl.textContent = cityName;
-
-//     cityEl.appendChild(titleEl);
-
-//     var statusEl = document.createElement('span');
-//     statusEl.classList = 'flex-row align-center';
-
-//     if (city[i].open_issues_count > 0) {
-//       statusEl.innerHTML =
-//         "<i class='fas fa-times status-icon icon-danger'></i>" + city[i].open_issues_count + ' issue(s)';
+// var getFeaturedCity = function (language) {
+//   var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey;
+//   fetch(apiUrl).then(function (response) {
+//     if (response.ok) {
+//       response.json().then(function (data) {
+//         displayCity(data.items, language);
+//       });
 //     } else {
-//       statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+//       alert('Error: ' + response.statusText);
 //     }
-
-//     cityEl.appendChild(statusEl);
-
-//     cityContainerEl.appendChild(cityEl);
-//   }
+//   }); ``
 // };
+
+var displayCity = function (city, searchTerm) {
+  // if (city.length === 0) {
+  //   weatherContainerEl.textContent = 'City not found.';
+  //   return;
+  // }
+
+  citySearchTerm.textContent = searchTerm;
+
+  for (var i = 0; i < city.length; i++) {
+    var cityName = city[i].weather + '/' + city[i].name;
+
+    var cityEl = document.createElement('a');
+    cityEl.classList = 'list-item flex-row justify-space-between align-center';
+    cityEl.setAttribute('href', './single-repo.html?repo=' + cityName);
+
+    var titleEl = document.createElement('span');
+    titleEl.textContent = cityName;
+
+    cityEl.appendChild(titleEl);
+
+    var statusEl = document.createElement('span');
+    statusEl.classList = 'flex-row align-center';
+
+    // if (city[i].weather > 0) {
+    //   statusEl.innerHTML =
+    //     "<i class='fas fa-times status-icon icon-danger'></i>" + city[i].weather + ' issue(s)';
+    // } else {
+    //   statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+    // }
+
+    cityEl.appendChild(statusEl);
+
+    weatherContainerEl.appendChild(cityEl);
+  }
+};
 
 userFormEl.addEventListener('submit', formSubmitHandler);
