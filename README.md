03dc43afe50a89da69dfb9ca0cb50acc77ab1542
emilygknight <emilyknight819@gmail.com>
2023-09-18 23:02:45 -0500

    finishing read me and trying to fix js file

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # weather-challenge
 
-## Recources 
+## Description
+
+- This app allows you to type in a city and get a link to that city's weather
+
+## Installation
+- 
+
+## Usage
+
+- You can use this app to get the current weather of the city you want
+
+## Features
+
+- You can type in any city in the search bar to get the weather of that city through a link
--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ var userFormEl = document.querySelector('#user-form');
 var cityInput = document.querySelector('#city');
 var weatherContainer = document.querySelector('#weather-container');
 var citySearchTerm = document.querySelector('#city-search-term');
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+// const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
 const APIKey = "0d9571e1b0c1a878cfe2800320169226";
 const city = '';
 
@@ -44,29 +44,25 @@ var getUserCity = function (city) {
 
 
 
-var displayCity = function (data, weather) {
+var displayCity = function (data, city) {
 
   citySearchTerm.textContent = city;
 
-  for (var i = 0; i < city.length; i++) {
-
-    var cityEl = document.createElement('a');
-    cityEl.classList = 'list-item flex-row justify-space-between align-center';
-    cityEl.setAttribute('href', 'https://api.openweathermap.org/data/2.5/weather?q=' + city.data + '&appid=' + APIKey);
+    cityName = document.createElement('a');
+    cityName.classList = 'list-item flex-column justify-space-between align-center';
+    cityName.setAttribute('href', 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey);
 
     var titleEl = document.createElement('span');
-    titleEl.textContent = cityInput;
+    titleEl.textContent = "Click here for Weather";
 
-    cityEl.appendChild(titleEl);
-    weatherContainer.appendChild(cityEl);
+    cityName.appendChild(titleEl);
+    weatherContainer.appendChild(cityName);
+    
+    console.log(city);
+    console.log(data);
   }
-
-
-console.log(city);
-console.log(weather);
-console.log(data);
-}
-
 };
+
+// };
 
 userFormEl.addEventListener('submit', formSubmitHandler);
